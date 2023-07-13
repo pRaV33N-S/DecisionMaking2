fa40e1abb023f5bcb4fc3ea621867604878da432
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-13 14:53:21 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionMaking2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "papaJohn";
+            string passWord = "afraidOfMamaJohn";
+            int loop = 3;
+            do
+            {
+                Console.WriteLine("Enter the UserName");
+                string givenUserName = Console.ReadLine();
+                Console.WriteLine("Enter the Password");
+                string givenPassword = Console.ReadLine();
+                
+                if (userName.Equals(givenUserName) && passWord.Equals(givenPassword))
+                {
+                    Console.WriteLine("Login Successfull");
+                    break;
+                }
+                else if (userName.Equals(givenUserName) && !passWord.Equals(givenPassword))
+                {
+                    Console.WriteLine("Invalid Password");
+                    loop--;
+                    Console.WriteLine($"You have {loop} remaining attempts");
+                }
+                else if (!userName.Equals(givenUserName) && passWord.Equals(givenPassword))
+                {
+                    Console.WriteLine("Invalid UserName");
+                    loop--;
+                    Console.WriteLine($"You have {loop} remaining attempts");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid UserName and Password");
+                    loop--;
+                    Console.WriteLine($"You have {loop} remaining attempts");
+                }
+            } while (loop > 0);
+            Console.WriteLine("Login Failed");
+        }
+    }
+}
